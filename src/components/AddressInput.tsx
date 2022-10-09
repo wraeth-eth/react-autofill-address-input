233ff04ceb48fa176eddeeef96f6e94dd3ac7294
Wraeth <wraeth.dao@gmail.com>
2022-10-09 14:01:50 +1100

    init

---
+++ src/components/AddressInput.tsx
@@ -0,0 +1,8 @@
+import { DetailedHTMLProps, InputHTMLAttributes } from 'react'
+
+export const AddressInput = (
+  props: DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+) => {
+  return <input {...props} />
+  //
+}
